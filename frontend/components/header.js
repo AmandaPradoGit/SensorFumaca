d8511341c90bd3f39c419eeb2e0922acfc781abd
AmandaPradoGit <amandapradogc@gmail.com>
2025-11-07 20:46:30 -0300

    Corrige espelhamento de imagem

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,17 +1,16 @@
 const header = document.createElement('header');
-header.className = 'fixed top-0 left-0 w-full z-50 bg-white'; 
+header.className = ' font-mono fixed top-0 left-0 w-full z-50 bg-white'; 
 header.innerHTML = `
-    <div class="bg-white-600 text-white p-8 shadow-md">
+    <div class="bg-orange-200 text-white p-8 shadow-md">
       <div class="container mx-auto flex justify-between items-center">
         
         <div class="flex items-center">
-          <img src="../imgs/icon.png" alt="icon" class="w-24 h-10 md:w-16 " />
+          <img src="imgs/icon.png" alt="icon" class="w-24 h-10 md:w-24 " />
         </div>
         
         <div class="flex items-center space-x-6 md:space-x-8">
           <a href="../frontend/dashboard.html" class="text-black hover:text-gray-700 text-sm md:text-base">Dashboard</a>
-          <p class="text-black text-sm md:text-base">Olá, Nome!</p>
-          <img src="../imgs/profilepic.png" alt="Perfil" class="w-6 h-6 md:w-8 md:h-8 rounded-full" />
+          <img src="imgs/profilepic.png" alt="Perfil" class="w-6 h-6 md:w-8 md:h-8 rounded-full" />
         </div>
         
       </div>
@@ -24,7 +23,7 @@ header.innerHTML = `
   if (profilePic) {
     profilePic.style.cursor = 'pointer'; 
     profilePic.addEventListener('click', () => {
-      window.location.href = 'profile.php';
+      window.location.href = 'profile.html';
     });
   }
 
@@ -32,7 +31,7 @@ header.innerHTML = `
   if (icon) {
     icon.style.cursor = 'pointer';
     icon.addEventListener('click', () => {
-      window.location.href = 'home.php';
+      window.location.href = 'index.html';
     });
   }
 });
