ff0128f1ac951ca03c98f4122548810f63d28bbe
AmandaPradoGit <amandapradogc@gmail.com>
2025-11-07 22:21:41 -0300

    corrige o header.js para novo estilo

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,19 +1,16 @@
 const header = document.createElement('header');
-header.className = 'fixed top-0 left-0 w-full z-50 bg-white'; 
+header.className = 'fixed top-0 left-0 w-full z-50 bg-[#f3f3f3]'; 
 header.innerHTML = `
-    <div class="bg-white-600 text-white p-8 shadow-md">
-      <div class="container mx-auto flex justify-between items-center">
-        
-        <div class="flex items-center">
-          <img src="../imgs/icon.png" alt="icon" class="w-24 h-10 md:w-16 " />
-        </div>
-        
-        <div class="flex items-center space-x-6 md:space-x-8">
+    <div class="bg-[#f3f3f3] text-white p-8">
+        <nav class="max-w-7xl mx-auto flex justify-between items-center">
+            <a href="#" class="flex items-center">
+                <img src="imgs/icon.png" alt="icon" class="h-8">
+            </a>
+            <div class="flex items-center space-x-6 md:space-x-8">
           <a href="../frontend/dashboard.html" class="text-black hover:text-gray-700 text-sm md:text-base">Dashboard</a>
-          <p class="text-black text-sm md:text-base">Olá, Nome!</p>
-          <img src="../imgs/profilepic.png" alt="Perfil" class="w-6 h-6 md:w-8 md:h-8 rounded-full" />
+          <img src="imgs/profilepic.png" alt="Perfil" class="w-6 h-6 md:w-8 md:h-8 rounded-full" />
         </div>
-        
+        </nav>
       </div>
     </div>`;
 
