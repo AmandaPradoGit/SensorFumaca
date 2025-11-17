f09ab36cea02dd3042a0f3f0ab4b07d1efa7c788
AmandaPradoGit <amandapradogc@gmail.com>
2025-11-16 22:06:27 -0300

    Arruma a conexao para aceitar ip do celular

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,6 @@ app.post('/logout', (req, res) => {
 app.post('/cadastro', userController.register);
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT,  '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
